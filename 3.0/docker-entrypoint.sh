24255f1283096ccafa35e42802a0d8a10b2fbdc7
Alexander Sadovsky <alino.sadovsky@gmail.com>
2016-09-21 16:10:22 +0200

    Update docker-entrypoint.sh

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -23,21 +23,21 @@ createAdminAccounts() {
   mongod --port 27017 --replSet "rs" --fork --logpath /data/db/mongodb.log
   sleep 8
   echo 2222222
-  export SITE_USR_ADMIN_PWD='xxx'
-  echo 3333333
+  export SITE_USR_ADMIN_PWD='xxx';
+  export SITE_KUKNITO_PWD='aaa';
+  export SITE_OPLOGREADER_PWD='ooo';
   export SITE_ROOT_PWD='yyy';
-  echo 4444444
+  echo 3333333
   mongo admin --eval "db.createUser({user:'siteUserAdmin', pwd:'$SITE_USR_ADMIN_PWD',roles: [{role:'userAdminAnyDatabase', db:'admin'}, 'readWrite' ]})";
-  echo 5555555
-  sleep 3
+  mongo admin --eval "db.createUser({user:'kuknito', pwd:'$SITE_KUKNITO_PWD',roles: [{role:'readWrite', db:'kuknito'}]})";
+  mongo admin --eval "db.createUser({user:'oplogReader', pwd:'$SITE_OPLOGREADER_PWD',roles: [{role:'read', db:'local'}]})";
   mongo admin --eval "db.createUser({user:'siteRootAdmin', pwd:'$SITE_ROOT_PWD',roles: [{role:'root', db:'admin'}, 'readWrite' ]})";
-  echo 6666666
+  echo 4444444
   sleep 3
   echo "exiting mongo (will start again)";
-  echo 7777777
   mongo admin --eval "db.shutdownServer({ force: true })";
   sleep 3
-  echo 8888888
+  echo 5555555
   echo "admin accounts created";
 }
 
