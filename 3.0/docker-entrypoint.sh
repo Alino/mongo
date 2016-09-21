e37297bf0c9e173ff47b7ba538e044eede05bf98
Alexander Sadovsky <alino.sadovsky@gmail.com>
2016-09-21 14:37:09 +0200

    Update docker-entrypoint.sh

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 set -e
 
+if [ "${1:0:1}" = '-' ]; then
+	set -- mongod "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
+	chown -R mongodb /data/configdb /data/db
+	exec gosu mongodb "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'mongod' ]; then
+	numa='numactl --interleave=all'
+	if $numa true &> /dev/null; then
+		set -- $numa "$@"
+	fi
+fi
+
 createAdminAccounts() {
   echo "creating admin accounts";
-  gosu mongodb bash -c 'touch /data/mongodb.log'
-  gosu mongodb bash -c 'mongod --port 27017 --fork --logpath /data/mongodb.log'
+  mongod --port 27017 --fork --logpath /data/db/mongodb.log
   sleep 8
   echo 2222222
   export SITE_USR_ADMIN_PWD='xxx'
@@ -24,23 +40,6 @@ createAdminAccounts() {
   echo 8888888
   echo "admin accounts created";
 }
-
-if [ "${1:0:1}" = '-' ]; then
-	set -- mongod "$@"
-fi
-
-# allow the container to be started with `--user`
-if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
-	chown -R mongodb /data/configdb /data/db
-	exec gosu mongodb "$BASH_SOURCE" "$@"
-fi
-
-if [ "$1" = 'mongod' ]; then
-	numa='numactl --interleave=all'
-	if $numa true &> /dev/null; then
-		set -- $numa "$@"
-	fi
-fi
 
 createAdminAccounts
 sleep 8
