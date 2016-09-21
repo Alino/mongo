2660b069976bb3187a38e2c59ab4e2ffe30925b0
Alexander Sadovsky <alino.sadovsky@gmail.com>
2016-09-21 13:50:43 +0200

    Update docker-entrypoint.sh

--- 3.0/docker-entrypoint.sh
+++ 3.0/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 createAdminAccounts() {
   echo "creating admin accounts";
   mongod --port 27017 --fork --logpath /var/log/mongod.log
+  sleep 8
   echo 2222222
   export SITE_USR_ADMIN_PWD='xxx'
   echo 3333333
@@ -11,11 +12,14 @@ createAdminAccounts() {
   echo 4444444
   mongo admin --eval "db.createUser({user:'siteUserAdmin', pwd:'$SITE_USR_ADMIN_PWD',roles: [{role:'userAdminAnyDatabase', db:'admin'}, 'readWrite' ]})";
   echo 5555555
+  sleep 3
   mongo admin --eval "db.createUser({user:'siteRootAdmin', pwd:'$SITE_ROOT_PWD',roles: [{role:'root', db:'admin'}, 'readWrite' ]})";
   echo 6666666
+  sleep 3
   echo "exiting mongo (will start again)";
   echo 7777777
   mongo admin --eval "db.shutdownServer()";
+  sleep 3
   echo 8888888
   echo "admin accounts created";
 }
@@ -38,4 +42,5 @@ if [ "$1" = 'mongod' ]; then
 fi
 
 createAdminAccounts
+sleep 8
 exec "$@"
